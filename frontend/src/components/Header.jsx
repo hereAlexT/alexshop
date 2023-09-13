de957b59eec5e7aa4b284413d5023c7d3d7b09b1
Alex T <79433869+hereAlexT@users.noreply.github.com>
2023-09-13 13:42:56 +1000

    cart

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Navbar, Nav, Container, Badge } from 'react-bootstrap'
 import { FaShoppingCart } from 'react-icons/fa'
 import { LinkContainer } from 'react-router-bootstrap'
+import { useSelector } from 'react-redux'
 import logo from '../assets/logo.png'
 
 
-
 const Header = () => {
+    const { cartItems } = useSelector((state) => state.cart);
+
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -22,8 +24,15 @@ const Header = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className='ms-auto'>
-                            <LinkContainer to="/cart">
-                                <Nav.Link><FaShoppingCart />Cart</Nav.Link>
+                            <LinkContainer to='/cart'>
+                                <Nav.Link>
+                                    <FaShoppingCart /> Cart
+                                    {cartItems.length > 0 && (
+                                        <Badge pill bg='success' style={{ marginLeft: '5px' }}>
+                                            {cartItems.reduce((a, c) => a + c.qty, 0)}
+                                        </Badge>
+                                    )}
+                                </Nav.Link>
                             </LinkContainer>
                             <LinkContainer to="/login">
                                 <Nav.Link >Sign In</Nav.Link>
