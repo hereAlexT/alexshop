53b06586706e8f6ef07815369a42e8c9d88214df
Alex T <79433869+hereAlexT@users.noreply.github.com>
2023-09-13 12:49:59 +1000

    message and loader components

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating';
 import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
+import Loader from '../components/Loader';
+import Message from "../components/Message";
 
 const ProductScreen = () => {
     const { id: productId } = useParams()
@@ -14,7 +16,7 @@ const ProductScreen = () => {
             <Link className='btn btn-light my-3' to='/'>
                 Go Back
             </Link>
-            {isLoading ? (<h2> Loading ...</h2>) : error ? (<div>{error?.data?.message || error.error}</div>) : (
+            {isLoading ? (<Loader />) : error ? (<Message variant="danger">{error?.data?.message || error.error}</Message>) : (
                 <>
                     <Row>
                         <Col md={5}>
