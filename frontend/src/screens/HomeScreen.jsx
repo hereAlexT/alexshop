a134f4a48ddd6a9f65f31cc634858948d58e6a55
Alex T <79433869+hereAlexT@users.noreply.github.com>
2023-09-14 14:41:07 +1000

    auth and login screen

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -6,7 +6,8 @@ import Loader from '../components/Loader';
 import Message from '../components/Message';
 
 const HomeScreen = () => {
-    const { data: products, isLoading, error } = useGetProductsQuery();
+    const { data, isLoading, error } = useGetProductsQuery();
+
 
     return (
         <>
@@ -18,7 +19,7 @@ const HomeScreen = () => {
                 <>
                     <h1> Latest Products </h1 >
                     <Row>
-                        {products.map((product) => (
+                        {data.products.map((product) => (
                             <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                                 <Product product={product} />
                             </Col>
